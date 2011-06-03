ac5a438af22ed99613ed1090e1e792cdab89d3aa
triplefox <triplefox@localhost>
2011-06-03 02:43:32 -0700

    map generation prototype using diamond-square midpoint displacement algorithm

---
+++ feature_server/commands.py
@@ -0,0 +1,345 @@
+# Copyright (c) Mathias Kaerlev 2011.
+
+# This file is part of pyspades.
+
+# pyspades is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pyspades is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pyspades.  If not, see <http://www.gnu.org/licenses/>.
+
+import inspect
+
+class InvalidPlayer(Exception):
+    pass
+
+class InvalidTeam(Exception):
+    pass
+
+def admin(func):
+    def new_func(connection, *arg, **kw):
+        if not connection.admin:
+            return 'No administrator rights!'
+        func(connection, *arg, **kw)
+    new_func.func_name = func.func_name
+    new_func.admin = True
+    return new_func
+
+def name(name):
+    def dec(func):
+        func.func_name = name
+        return func
+    return dec
+
+def get_player(protocol, value):
+    try:
+        if value.startswith('#'):
+            value = int(value[1:])
+            return protocol.players[value][0]
+        players = protocol.players
+        try:
+            return players[value][0]
+        except KeyError:
+            value = value.lower()
+            for player in players.values():
+                if player.name.lower().count(value):
+                    return player
+    except (KeyError, IndexError, ValueError):
+        pass
+    raise InvalidPlayer()
+
+def get_team(connection, value):
+    value = value.lower()
+    if value == 'blue':
+        return connection.protocol.blue_team
+    elif value == 'green':
+        return connection.protocol.green_team
+    raise InvalidTeam()
+
+def join_arguments(arg, default = None):
+    if not arg:
+        return default
+    return ' '.join(arg)
+
+@admin
+def kick(connection, value, *arg):
+    reason = join_arguments(arg)
+    player = get_player(connection.protocol, value)
+    player.kick(reason)
+
+@admin
+def ban(connection, value, *arg):
+    reason = join_arguments(arg)
+    player = get_player(connection.protocol, value)
+    player.ban(reason)
+
+@admin
+def say(connection, *arg):
+    value = ' '.join(arg)
+    connection.protocol.send_chat(value)
+    connection.protocol.irc_say(value)
+
+@admin
+def kill(connection, value):
+    player = get_player(connection.protocol, value)
+    player.kill()
+    message = '%s killed %s' % (connection.name, player.name)
+    connection.protocol.send_chat(message, irc = True)
+
+@admin
+def heal(connection, player = None):
+    if player is not None:
+        player = get_player(connection.protocol, player)
+        message = '%s was healed by %s' % (player.name, connection.name)
+    else:
+        player = connection
+        message = '%s was healed' % (connection.name)
+    player.refill()
+    connection.protocol.send_chat(message, irc = True)
+
+def votekick(connection, value):
+    player = get_player(connection.protocol, value)
+    return connection.protocol.start_votekick(connection, player)
+
+@name('y')
+def vote_yes(connection):
+    connection.protocol.votekick(connection, True)
+
+@name('n')
+def vote_no(connection):
+    connection.protocol.votekick(connection, False)
+
+@name('cancel')
+def cancel_vote(connection):
+    return connection.protocol.cancel_votekick(connection)
+
+def rules(connection):
+    lines = connection.protocol.rules
+    if lines is None:
+        return
+    connection.send_lines(lines)
+
+def help(connection):
+    """
+    This help
+    """
+    names = [command.func_name for command in command_list
+        if hasattr(command, 'admin') in (connection.admin, False)]
+    return 'Available commands: %s' % (', '.join(names))
+
+def login(connection, password):
+    """
+    Login as admin
+    """
+    passwords = connection.protocol.admin_passwords
+    if password in passwords:
+        connection.admin = True
+        message = '%s logged in as admin' % connection.name
+        connection.protocol.send_chat(message, irc = True)
+        return None
+    if connection.login_retries is None:
+        connection.login_retries = connection.protocol.login_retries - 1
+    else:
+        connection.login_retries -= 1
+    if not connection.login_retries:
+        connection.kick('Ran out of login attempts')
+        return
+    return 'Invalid password - you have %s tries left' % (
+        connection.login_retries)
+
+def pm(connection, value, *arg):
+    player = get_player(connection.protocol, value)
+    message = join_arguments(arg)
+    player.send_chat('PM from %s: %s' % (connection.name, message))
+    return 'PM sent to %s' % player.name
+
+def follow(connection, value):
+    """
+    Follow a player; on your next spawn, you'll spawn at their position,
+    similar to the squad spawning feature of Battlefield.
+    """
+    player = get_player(connection.protocol, value)
+    curfollowed = player.get_followers()
+    # TODO - server option for follow limits
+    followlimit = connection.protocol.max_followers
+    if connection is player:
+        return "You can't follow yourself!"
+    if not connection.team is player.team:
+        return '%s is not on your team.' % (player.name)
+    if connection.follow is player:
+        return "You're already following %s" % (player.name)
+    if len(curfollowed) >= followlimit:
+        return '%s has too many followers!' % (player.name)
+    connection.follow = player
+    return 'Next time you die you will spawn where %s is.' % (player.name)
+
+def unfollow(connection):
+    if connection.follow is not None:
+        message = 'You are no longer following %s.' % (connection.follow.name)
+        connection.follow = None
+        return message
+
+@admin
+def lock(connection, value):
+    team = get_team(connection, value)
+    team.locked = True
+    connection.protocol.send_chat('%s team is now locked' % team.name)
+    connection.protocol.irc_say('* %s locked %s team' % (connection.name, 
+        team.name))
+
+@admin
+def unlock(connection, value):
+    team = get_team(connection, value)
+    team.locked = False
+    connection.protocol.send_chat('%s team is now unlocked' % team.name)
+    connection.protocol.irc_say('* %s unlocked %s team' % (connection.name, 
+        team.name))
+
+@name('setbalance')
+@admin
+def set_balance(connection, value):
+    try:
+        value = int(value)
+    except ValueError:
+        return 'Invalid value %r. Use 0 for off, 1 and up for on' % value
+    protocol = connection.protocol
+    protocol.balanced_teams = value
+    protocol.send_chat('Balanced teams set to %s' % value)
+    connection.protocol.irc_say('* %s set balanced teams to %s' % (
+        connection.name, value))
+
+@name('togglebuild')
+@admin
+def toggle_build(connection):
+    value = not connection.protocol.building
+    connection.protocol.building = value
+    on_off = ['OFF', 'ON'][int(value)]
+    connection.protocol.send_chat('Building has been toggled %s!' % on_off)
+    connection.protocol.irc_say('* %s toggled building %s' % (connection.name, 
+        on_off))
+    
+@name('togglekill')
+@admin
+def toggle_kill(connection):
+    value = not connection.protocol.killing
+    connection.protocol.killing = value
+    on_off = ['OFF', 'ON'][int(value)]
+    connection.protocol.send_chat('Killing has been toggled %s!' % on_off)
+    connection.protocol.irc_say('* %s toggled killing %s' % (connection.name, 
+        on_off))
+
+@name('toggleteamkill')
+@admin
+def toggle_teamkill(connection):
+    value = not connection.protocol.friendly_fire
+    connection.protocol.friendly_fire = value
+    on_off = ['OFF', 'ON'][int(value)]
+    connection.protocol.send_chat('Friendly fire has been toggled %s!' % on_off)
+    connection.protocol.irc_say('* %s toggled friendly fire %s' % (
+        connection.name, on_off))
+
+@admin
+def mute(connection, value):
+    player = get_player(connection.protocol, value)
+    player.mute = True
+    message = '%s has been muted by %s' % (player.name, connection.name)
+    connection.protocol.send_chat(message, irc = True)
+
+@admin
+def unmute(connection, value):
+    player = get_player(connection.protocol, value)
+    player.mute = False
+    message = '%s has been unmuted by %s' % (player.name, connection.name)
+    connection.protocol.send_chat(message, irc = True)
+
+from pyspades.server import position_data
+
+@admin
+def teleport(connection, player1, player2 = None):
+    player1 = get_player(connection.protocol, player1)
+    if player2 is not None:
+        player, target = player1, get_player(connection.protocol, player2)
+        message = '%s teleported %s to %s' % (connection.name, player.name, 
+            target.name)
+    else:
+        player, target = connection, player1
+        message = '%s teleported to %s' % (connection.name, target.name)
+    
+    player.set_position(target.get_position())
+    player.send_contained(position_data)
+    connection.protocol.send_chat(message, irc = True)
+
+@admin
+def god(connection, value = None):
+    if value is not None:
+        connection = get_player(connection.protocol, value)
+    connection.god = not connection.god
+    if connection.god:
+        message = '%s entered GOD MODE!' % connection.name
+    else:
+        message = '%s returned to being a mere human.' % connection.name
+    connection.protocol.send_chat(message, irc = True)
+
+@admin
+def reset_game(connection):
+    connection.reset_game()
+    connection.protocol.send_chat('Game has been reset by %s' % (
+        connection.name), irc = True)
+    
+command_list = [
+    help,
+    pm,
+    login,
+    kick,
+    votekick,
+    vote_yes,
+    vote_no,
+    cancel_vote,
+    ban,
+    mute,
+    unmute,
+    say,
+    kill,
+    heal,
+    lock,
+    unlock,
+    set_balance,
+    rules,
+    toggle_build,
+    toggle_kill,
+    toggle_teamkill,
+    teleport,
+    god,
+    follow,
+    unfollow,
+    reset_game
+]
+
+commands = {}
+
+for command_func in command_list:
+    commands[command_func.func_name] = command_func
+
+def handle_command(connection, command, parameters):
+    command = command.lower()
+    try:
+        command_func = commands[command]
+    except KeyError:
+        return 'Invalid command'
+    try:
+        return command_func(connection, *parameters)
+    except TypeError:
+        return 'Invalid number of arguments for %s' % command
+    except InvalidPlayer:
+        return 'No such player'
+    except InvalidTeam:
+        return 'Invalid team specifier'
+    except ValueError:
+        return 'Invalid parameters'
---
+++ feature_server/irc.py
@@ -0,0 +1,222 @@
+# Copyright (c) Mathias Kaerlev 2011.
+
+# This file is part of pyspades.
+
+# pyspades is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pyspades is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pyspades.  If not, see <http://www.gnu.org/licenses/>.
+
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol
+from pyspades.constants import MAX_CHAT_SIZE
+from pyspades.common import encode, decode
+from commands import get_player, join_arguments, InvalidPlayer, InvalidTeam
+
+import random
+import string
+
+MAX_IRC_CHAT_SIZE = MAX_CHAT_SIZE * 2
+
+def channel(func):
+    def new_func(self, user, channel, *arg, **kw):
+        if not channel == self.factory.channel:
+            return
+        user = user.split('!', 1)[0]
+        func(self, user, channel, *arg, **kw)
+    return new_func
+
+class IRCBot(irc.IRCClient):
+    ops = None
+    voices = None
+    
+    def _get_nickname(self):
+        return self.factory.nickname
+    nickname = property(_get_nickname)
+    
+    def signedOn(self):
+        self.join(self.factory.channel)
+    
+    def joined(self, channel):
+        if channel == self.factory.channel:
+            self.ops = []
+            self.voices = []
+        print "Joined channel %s" % channel
+    
+    def irc_RPL_NAMREPLY(self, *arg):
+        if not arg[1][2] == self.factory.channel:
+            return
+        for name in arg[1][3].split():
+            mode = name[0]
+            l = {'@':self.ops, '+':self.voices}
+            if mode in l: l[mode].append(name[1:])
+    
+    def left(self, channel):
+        if channel == self.factory.channel:
+            self.ops = None
+            self.voices = None
+    
+    @channel
+    def modeChanged(self, user, channel, set, modes, args):
+        ll = {'o' : self.ops, 'v' : self.voices}
+        for i in range(len(args)):
+            mode, name = modes[i], args[i]
+            if mode not in ll:
+                continue
+            l = ll[mode]
+            if name not in l and set:
+                l.append(name)
+            elif name in l and not set:
+                l.remove(name)
+    
+    @channel
+    def privmsg(self, user, channel, msg):
+        if (user in self.ops) or (user in self.voices):
+            prefixed_username = ('@' if user in self.ops else '+') + user
+            if msg.startswith(self.factory.commandprefix):
+                params = msg[len(self.factory.commandprefix):].split()
+                result = handle_command(self, prefixed_username, params[0], params[1:])
+                if result is not None:
+                    self.send(result)
+            else:
+                max_len = MAX_IRC_CHAT_SIZE - len(self.protocol.server_prefix) - 1
+                message = ("<%s> %s" % (prefixed_username, msg))[:max_len]
+                self.factory.server.log(message)
+                self.factory.server.send_chat(encode(message))
+    
+    @channel
+    def userLeft(self, user, channel):
+        if user in self.ops:
+            self.ops.remove(user)
+        if user in self.voices:
+            self.voices.remove(user)
+    
+    def send(self, msg):
+        self.msg(self.factory.channel, msg)
+    
+    def me(self, msg):
+        self.describe(self.factory.channel, msg)
+
+class IRCClientFactory(protocol.ClientFactory):
+    protocol = IRCBot
+    lost_reconnect_delay = 20
+    failed_reconnect_delay = 60
+    bot = None
+    
+    def __init__(self, server, config):
+        self.server = server
+        self.nickname = config.get('nickname', 
+            'pyspades%s' % random.randrange(0, 99)).encode('ascii')
+        self.username = config.get('username', 'pyspades').encode('ascii')
+        self.realname = config.get('realname', server.name).encode('ascii')
+        self.channel = config.get('channel', "#pyspades.bots").encode('ascii')
+        self.commandprefix = config.get('commandprefix', '.').encode('ascii')
+    
+    def startedConnecting(self, connector):
+        print "Connecting to IRC server..."
+    
+    def clientConnectionLost(self, connector, reason):
+        print "Lost connection to IRC server (%s), reconnecting in %s seconds" % (
+            reason, self.lost_reconnect_delay)
+        reactor.callLater(self.lost_reconnect_delay, connector.connect)
+    
+    def clientConnectionFailed(self, connector, reason):
+        print "Could not connect to IRC server (%s), retrying in %s seconds" % (
+            reason, self.failed_reconnect_delay)
+        reactor.callLater(self.failed_reconnect_delay, connector.connect)
+    
+    def buildProtocol(self, address):
+        p = self.protocol()
+        p.factory = self
+        p.protocol = self.server
+        self.bot = p
+        return p
+
+class IRCRelay(object):
+    factory = None
+    
+    def __init__(self, protocol, config):
+        self.factory = IRCClientFactory(protocol, config)
+        reactor.connectTCP(config.get('server'), config.get('port', 6667), 
+            self.factory)
+    
+    def send(self, msg):
+        self.factory.bot.send(msg)
+    
+    def me(self, msg):
+        self.factory.bot.describe(msg)
+
+def admin(func):
+    def new_func(bot, user, *arg, **kw):
+        if not user[0] == '@':
+            return
+        func(bot, user, *arg, **kw)
+    new_func.func_name = func.func_name
+    return new_func
+
+@admin
+def mute(bot, user, value):
+    player = get_player(bot.protocol, value)
+    player.mute = True
+    message = '%s has been muted by %s' % (player.name, user)
+    bot.protocol.send_chat(message, irc = True)
+
+@admin
+def unmute(bot, user, value):
+    player = get_player(bot.protocol, value)
+    player.mute = False
+    message = '%s has been unmuted by %s' % (player.name, user)
+    bot.protocol.send_chat(message, irc = True)
+
+@admin
+def kick(bot, user, value, *arg):
+    reason = join_arguments(arg)
+    player = get_player(bot.protocol, value)
+    player.kick(reason)
+
+def who(bot, user):
+    names = [conn.name for conn in bot.factory.server.players.values()]
+    c = len(names)
+    msg = "has %s player%s connected" % ("no" if c == 0 else c,
+        "" if c == 1 else "s")
+    if c > 0:
+        names.sort()
+        msg += ": %s" % (', '.join(names))
+    bot.me(msg)
+
+command_list = [
+    mute,
+    unmute,
+    kick,
+    who
+]
+
+commands = {}
+
+for command_func in command_list:
+    commands[command_func.func_name] = command_func
+
+def handle_command(bot, user, command, parameters):
+    command = command.lower()
+    try:
+        command_func = commands[command]
+    except KeyError:
+        return #'Invalid command'
+    try:
+        return command_func(bot, user, *parameters)
+    except TypeError:
+        return '%s: Invalid number of arguments for %s' % (user[1:], command)
+    except InvalidPlayer:
+        return '%s: No such player' % user[1:]
+    except InvalidTeam:
+        return '%s: Invalid team specifier' % user[1:]
+    except ValueError:
+        return '%s: Invalid parameters' % user[1:]
