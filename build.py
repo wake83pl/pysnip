32daad7ee60223b8fe40f9d777750f8225883363
triplefox <triplefox@localhost>
2011-06-25 22:58:10 -0700

    change debug logger: attempt to find leaking references

---
+++ build.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
+ext_modules = []
+
+names = [
+    'pyspades.compression',
+    'pyspades.load',
+    'pyspades.compressedvxl',
+    'pyspades.bytes',
+    'pyspades.packet',
+    'pyspades.loaders',
+    'pyspades.serverloaders',
+    'pyspades.clientloaders'
+]
+
+for name in names:
+    ext_modules.append(Extension(name, ['./%s.pyx' % name.replace('.', '/')],
+        language = 'c++'))
+
+setup(
+    name = 'pyspade extensions',
+    ext_modules = cythonize(ext_modules)
+)
