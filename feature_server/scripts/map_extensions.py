e6d05554b4c2907755dd880f9fc112cb88fcef90
duckslingers <duckslingers@gmail.com>
2011-07-18 11:45:20 -0300

    Added /where, /move, /changemap

---
+++ feature_server/scripts/map_extensions.py
@@ -0,0 +1,27 @@
+from pyspades.server import hit_packet
+
+def apply_script(protocol, connection, config):
+    
+    class MapExtensionConnection(connection):
+        def on_position_update(self):
+            extensions = self.protocol.map_info.extensions
+            if (extensions.has_key('water_damage') and
+                self.world_object.position.z>=61):
+                water_damage = extensions['water_damage']                
+                self.environment_hit(water_damage)
+            connection.on_position_update(self)
+    
+        def environment_hit(self, value):
+            if value<0 and self.hp >= 100: # do nothing at max health
+                return
+            self.hp -= value
+            if self.hp <= 0:
+                self.kill()
+                return
+            if self.hp > 100: # limit health gains
+                self.hp = 100
+            hit_packet.hp = self.hp
+            hit_packet.sound = True
+            self.send_contained(hit_packet)
+
+    return protocol, MapExtensionConnection
