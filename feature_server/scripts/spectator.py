411ce81840156338d4e3745ca84cf16735b0e46f
matpow2 <matpow2@localhost>
2011-07-31 22:37:22 +0200

    Fix melee damage

---
+++ feature_server/scripts/spectator.py
@@ -0,0 +1,129 @@
+from pyspades.server import player_data, chat_message
+from pyspades.server import create_player, position_data, kill_action
+from commands import add, rights, admin, name, get_player
+import commands
+
+@admin
+def god(connection, value = None):
+    if value is None and connection.spectator:
+        return "You're spectating"
+    elif value is not None:
+        value = get_player(connection.protocol, value)
+        if value.spectator:
+            return '%s is a spectator.' % value.name
+    return commands.god(connection, value)
+
+@admin
+def invisible(connection, player = None):
+    if player is None and connection.spectator:
+        return "You're spectating"
+    elif player is not None:
+        player = get_player(connection.protocol, player)
+        if player.spectator:
+            return '%s is a spectator.' % player.name
+    return commands.invisible(connection, player)
+
+@name('togglekill')
+@admin
+def toggle_kill(connection, player = None):
+    if player is not None:
+        player_ = get_player(connection.protocol, player)
+        if player_.spectator:
+            return '%s is a spectator.' % connection.name
+    return commands.toggle_kill(connection, player)
+
+@name('togglebuild')
+@admin
+def toggle_build(connection, player = None):
+    if player is not None:
+        player_ = get_player(connection.protocol, player)
+        if player_.spectator:
+            return '%s is a spectator.' % connection.name
+    return commands.toggle_build(connection, player)
+
+@name('nofollow')
+def no_follow(connection):
+    if connection.spectator:
+        return "You're spectating."
+    return commands.no_follow(connection)
+
+def spectators(connection):
+    names = [p.name for p in connection.protocol.players.values()
+        if p.spectator]
+    if len(names):
+        return '%s %s spectating.' % (', '.join(names), 'is' if len(names) == 1
+            else 'are')
+    else:
+        return 'There are no spectators.'
+
+for func in (god, invisible, toggle_build, toggle_kill, no_follow, spectators):
+    add(func)
+
+rights['spectator'] = ['tp', 'goto']
+
+def apply_script(protocol, connection, config):
+    class SpectatorConnection(connection):
+        spectator = False
+        
+        def on_join(self):
+            for player in self.protocol.connections.values():
+                if player.spectator:
+                    player_data.player_left = player.player_id
+                    self.send_contained(player_data)
+            connection.on_join(self)
+        
+        def on_user_login(self, user_type):
+            if user_type != 'spectator':
+                return connection.on_user_login(self, user_type)
+            self.spectator = True
+            self.filter_visibility_data = True
+            self.god = True
+            self.invisible = True
+            self.killing = False
+            self.building = False
+            self.drop_flag()
+            self.drop_followers()
+            self.followable = False
+            self.follow = None
+            self.respawn_time = self.protocol.respawn_time
+            player_data.player_left = self.player_id
+            for player in self.protocol.connections.values():
+                if player is self:
+                    continue
+                if not player.spectator:
+                    player.send_contained(player_data)
+                else:
+                    pos = player.team.get_random_location(True)
+                    x, y, z = pos
+                    create_player.player_id = player.player_id
+                    create_player.name = player.name
+                    create_player.x = x
+                    create_player.y = y - 128
+                    create_player.weapon = player.weapon
+                    position_data.set((0, 0, 0), player.player_id)
+                    kill_action.not_fall = True
+                    kill_action.player1 = kill_action.player2 = player.player_id
+                    self.send_contained(create_player)
+                    self.send_contained(position_data)
+                    self.send_contained(kill_action)
+                    position_data.player_id = self.player_id
+                    kill_action.player1 = kill_action.player2 = self.player_id
+                    player.send_contained(position_data)
+                    player.send_contained(kill_action)
+            return connection.on_user_login(self, user_type)
+        
+        def on_chat(self, value, global_message):
+            if not self.spectator:
+                return connection.on_chat(self, value, global_message)
+            chat_message.global_message = global_message
+            chat_message.value = value
+            chat_message.player_id = self.player_id
+            for player in self.protocol.connections.values():
+                if player is not self and player.spectator:
+                    player.send_contained(chat_message)
+            message = '(%s) %s' % (self.name, value)
+            self.protocol.irc_say(message)
+            print message.encode('ascii', 'replace')
+            return False
+        
+    return protocol, SpectatorConnection
---
+++ pyspades/load.pyx
@@ -0,0 +1,211 @@
+# Copyright (c) Mathias Kaerlev 2011.
+
+# This file is part of pyspades.
+
+# pyspades is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pyspades is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pyspades.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyspades.common cimport allocate_memory
+
+cpdef tuple get_color_tuple(int color):
+    cdef int r, g, b, a
+    b = color & 0xFF
+    g = (color & 0xFF00) >> 8
+    r = (color & 0xFF0000) >> 16
+    a = (((color & 0xFF000000) >> 24) / 128.0) * 255
+    return (r, g, b, a)
+
+cdef inline int make_color(int r, int g, int b, a):
+    return b | (g << 8) | (r << 16) | (<int>((a / 255.0) * 128) << 24)
+
+import random
+import time
+
+cdef inline int get_z(int x, int y, MapData * map, int start = 0):
+    cdef int z
+    for z in range(start, 64):
+        if get_solid(x, y, z, map):
+            return z
+    return 0
+
+cdef class Generator:
+    cdef MapGenerator * generator
+    cdef public:
+        bint done
+    
+    def __init__(self, VXLData data):
+        self.done = False
+        self.generator = create_map_generator(data.map)
+    
+    def get_data(self, int columns = 2):
+        if self.done:
+            return None
+        value = get_generator_data(self.generator, columns)
+        if not value:
+            self.done = True
+            return None
+        return value
+    
+    def __dealloc__(self):
+        delete_map_generator(self.generator)
+
+cdef class VXLData:
+    def __init__(self, fp = None):
+        cdef unsigned char * c_data
+        if fp is not None:
+            data = fp.read()
+            c_data = data
+        else:
+            c_data = NULL
+        self.map = load_vxl(c_data)
+    
+    def load_vxl(self, c_data = None):
+        self.map = load_vxl(c_data)
+    
+    def get_point(self, int x, int y, int z):
+        return (get_solid(x, y, z, self.map), get_color_tuple(get_color(
+            x, y, z, self.map)))
+    
+    def copy(self):
+        cdef VXLData map = VXLData()
+        map.map = copy_map(self.map)
+        return map
+    
+    cpdef int get_solid(self, int x, int y, int z):
+        if x < 0 or x >= 512 or y < 0 or y >= 512 or z < 0 or z >= 64:
+            return 0
+        return get_solid(x, y, z, self.map)
+    
+    cpdef int get_color(self, int x, int y, int z):
+        if x < 0 or x >= 512 or y < 0 or y >= 512 or z < 0 or z >= 64:
+            return 0
+        return get_color(x, y, z, self.map)
+    
+    cpdef int get_z(self, int x, int y, int start = 0):
+        return get_z(x, y, self.map, start)
+    
+    cpdef int get_height(self, int x, int y):
+        cdef int h_z
+        for h_z in range(63, -1, -1):
+            if not get_solid(x, y, h_z, self.map):
+                return h_z + 1
+        return 0
+    
+    def remove_point(self, int x, int y, int z, bint user = True, 
+                     bint no_collapse = False):
+        if x < 0 or x >= 512 or y < 0 or y >= 512 or z < 0 or z >= 64:
+            return
+        if user and z >= 62:
+            return
+        if not get_solid(x, y, z, self.map):
+            return
+        set_point(x, y, z, self.map, 0, 0)
+        if no_collapse:
+            return
+        start = time.time()
+        for node_x, node_y, node_z in self.get_neighbors(x, y, z):
+            if node_z >= 62:
+                continue
+            self.check_node(node_x, node_y, node_z, True)
+        taken = time.time() - start
+        if taken > 0.1:
+            print 'removing block at', x, y, z, 'took:', taken
+    
+    def remove_point_unsafe(self, int x, int y, int z):
+        set_point(x, y, z, self.map, 0, 0)
+            
+    cpdef bint has_neighbors(self, int x, int y, int z):
+        return (
+            self.get_solid(x + 1, y, z) or
+            self.get_solid(x - 1, y, z) or
+            self.get_solid(x, y + 1, z) or
+            self.get_solid(x, y - 1, z) or
+            self.get_solid(x, y, z + 1) or
+            self.get_solid(x, y, z - 1)
+        )
+    
+    cpdef bint is_surface(self, int x, int y, int z):
+        return (
+            not self.get_solid(x, y, z - 1) or
+            not self.get_solid(x, y, z + 1) or
+            not self.get_solid(x + 1, y, z) or
+            not self.get_solid(x - 1, y, z) or
+            not self.get_solid(x, y + 1, z) or
+            not self.get_solid(x, y - 1, z)
+        )
+    
+    cpdef list get_neighbors(self, int x, int y, int z):
+            cdef list neighbors = []
+            for (node_x, node_y, node_z) in ((x, y, z - 1),
+                                             (x, y - 1, z),
+                                             (x, y + 1, z),
+                                             (x - 1, y, z),
+                                             (x + 1, y, z),
+                                             (x, y, z + 1)):
+                if self.get_solid(node_x, node_y, node_z):
+                    neighbors.append((node_x, node_y, node_z))
+            return neighbors
+    
+    cpdef bint check_node(self, int x, int y, int z, bint destroy = False):
+        return check_node(x, y, z, self.map, destroy)
+    
+    cpdef bint set_point(self, int x, int y, int z, tuple color_tuple, 
+                         bint user = True):
+        if user and (z not in xrange(62) or not self.has_neighbors(x, y, z)):
+            return False
+        r, g, b, a = color_tuple
+        cdef int color = make_color(r, g, b, a)
+        set_point(x, y, z, self.map, 1, color)
+        return True
+    
+    cpdef bint set_point_unsafe(self, int x, int y, int z, tuple color_tuple):
+        r, g, b, a = color_tuple
+        cdef int color = make_color(r, g, b, a)
+        set_point(x, y, z, self.map, 1, color)
+        return True
+    
+    def set_point_unsafe_int(self, int x, int y, int z, int color):
+        set_point(x, y, z, self.map, 1, color)
+    
+    def get_overview(self):
+        cdef unsigned int * data
+        data_python = allocate_memory(sizeof(int[512][512]), <char**>&data)
+        cdef unsigned int i, x, y, z, r, g, b, color
+        i = 0
+        for y in range(512):
+            for x in range(512):
+                color = get_color(x, y, get_z(x, y, self.map), self.map)
+                b = color & 0xFF
+                g = (color & 0xFF00) >> 8
+                r = (color & 0xFF0000) >> 16
+                data[i] = r | (g << 8) | (b << 16) | (255 << 24)
+                i += 1
+        return data_python
+    
+    def generate(self):
+        start = time.time()
+        data = save_vxl(self.map)
+        dt = time.time() - start
+        if dt > 1.0:
+            print 'VXLData.generate() took %s' % (dt)
+        return data
+    
+    def get_generator(self):
+        return Generator(self)
+    
+    def __dealloc__(self):
+        cdef MapData * map
+        if self.map != NULL:
+            map = self.map
+            self.map = NULL
+            delete_vxl(map)
